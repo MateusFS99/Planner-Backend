710d456ed278c9c2e7d420491fad67c6a3dc4248
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-07-09 21:36:13 -0300

    - Feat: Banco de Dados Configurado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = fastify();
 
-app.get("/teste", () => {
-  return "aoba";
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: "Florianópolis",
+      starts_at: new Date(),
+      ends_at: new Date(),
+    },
+  });
+
+  return "Registro Cadastrado";
+});
+
+app.get("/listar", async () => {
+  return await prisma.trip.findMany();
 });
 
 app.listen({ port: 8000 }).then(() => {
