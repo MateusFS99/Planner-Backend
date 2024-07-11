11edc5012765c840f4772a5d47d36b6b3b6a8b3a
Mateus Fonseca <mateusfsantos.pfs@gmail.com>
2024-07-11 10:36:15 -0300

    - Feat: Endpoints de Participantes

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,11 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify();
 
@@ -24,12 +29,18 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipants);
+app.register(updateTrip);
+app.register(getTripDetails);
 
 app.register(createActivity);
 app.register(getActivities);
 
 app.register(createLink);
 app.register(getLinks);
+
+app.register(getParticipant);
+app.register(getParticipants);
+app.register(createInvite);
 
 app.listen({ port: 8000 }).then(() => {
   console.log("Server Running");
